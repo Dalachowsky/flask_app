41efebc21bcfe5fe93bf0edd3a2b4b4a2fa4ccd7
kdalach <kdalach@KDalach.lan>
2023-11-18 19:23:18 +0100

    Robobo fix gamma

--- src/flask_app/robobo/robobo.py
+++ src/flask_app/robobo/robobo.py
@@ -13,12 +13,19 @@ class robobo_arc:
         delta: float,
         a = None,
         A: float = 0.01,
-        I: float = 0.1):
+        I: float = 0.1,
+        centerPoint = (0,0),
+        startPoint = None):
 
         self.R = R
         self.delta = delta
         self.A = A
         self.I = I
+        self.centerPoint = centerPoint
+        if startPoint is not None:
+            self.startPoint = startPoint
+        else:
+            self.startPoint = (self.centerPoint[0] + self.R, self.centerPoint[1])
 
         self.beta = acos(R / (R+A))
         print(f"a = {a}")
@@ -28,21 +35,25 @@ class robobo_arc:
             self.a = ceil(self._a) if not (self._a).is_integer() else self._a
         else:
             self.a = a
-        self.alpha = abs((delta - 2*self.beta)/(2*self.a))
+        self.alpha = (abs(delta) - 2*abs(self.beta))/(2*self.a)
 
     def getPoints(self) -> List[Tuple[float, float]]:
-        points = [self.getPoint(n) for n in range(0,self.a+1)]
-        points.insert(0, (self.R, 0.0))
-        points.append((cos(self.delta)*self.R, sin(self.delta)*self.R))
+        points = [self.getPoint(n) for n in range(0,self.a+3)]
         return points
 
     def getPoint(self, n: int) -> Tuple[float, float]:
+        if n == 0:
+            return self.startPoint
+        if n == self.a + 2:
+            return ((self.R)*cos(self.getGamma(n)), (self.R)*sin(self.getGamma(n)))
         return ((self.R + self.A)*cos(self.getGamma(n)), (self.R + self.A)*sin(self.getGamma(n)))
 
     def getGamma(self, n: int):
-        if n == self.a+1:
+        if n == 0:
+            return 0
+        if n == self.a+2:
             return self.delta
-        return sign(self.delta) * (self.beta + n*2*self.alpha)
+        return sign(self.delta) * (self.beta + (n-1)*2*self.alpha)
 
     def getMathML(self):
         res = ""
@@ -57,11 +68,11 @@ class robobo_arc:
         res += mathml(f"\\alpha_{{rz}} = \\frac{{\\delta - 2\\beta}}{{2a}} = {round(degrees(self.alpha), 2)}^{{\\circ}}") + "<br>"
         res += '<table border="1px solid black">\n'
         res += "<tr><td>l.p</td><td>&gamma; [&deg;]</td><td>P</td></tr>\n"
-        for n in range(0,self.a+2):
+        for n, point in enumerate(self.getPoints()):
             res += "<tr>\n"
             res += f"<td>{n}</td>\n"
             res += f"<td>{round(degrees(self.getGamma(n)), 2)}</td>"
-            res += f"<td>({', '.join([str(round(i, 2)) for i in self.getPoint(n)])})</td>"
+            res += f"<td>({', '.join([str(round(i, 2)) for i in point])})</td>"
             res += "</tr>\n"
         res += "</table>"
 
